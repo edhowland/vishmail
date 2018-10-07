5949ad5997c220a9cc3f99933d90aad5b888e502
Ed Howland <ed.howland@gmail.com>
2018-10-07 15:30:20 -0400

    fixed require in compose.rb

--- compose.rb
+++ compose.rb
@@ -1,6 +1,8 @@
 # compose.rb - FFI interface to Viper Buffer class from Vish
 
 require_relative '../viper/lib/api'
+require_relative '../vish/lib/vish'
+
 require_relative 'buffer_ffi'
 
 Dispatch << BufferFFI
