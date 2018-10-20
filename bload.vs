4316762960fccd282ceea02f071d4ff7da156e91
Ed Howland <ed.howland@gmail.com>
2018-10-20 17:34:15 -0400

    removed bload.vs from RPL and compile targets

--- bload.vs
+++
@@ -1,8 +0,0 @@
-# bload.vs - test for buffer
-cb=cmove(mkbuf("line 1\nline 2\nline 3\nline 4\n"))
-#
- compose=repl(:ckeys, :cfmt, :cerr)
-
-
-# for testing:
-defn tk() { %readc | %spec_key }
--- h.sh
+++ h.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # h.sh - spin up Vish code to handle inbox.txt manipulation
-ivs -r ./util.rb -r ./msg.rb -r ./compose.rb fnlib.vs main.vs msg.vs inbox.vs insert.vs send.vs compose.vs  bload.vs
+ivs -r ./util.rb -r ./msg.rb -r ./compose.rb fnlib.vs main.vs msg.vs inbox.vs insert.vs send.vs compose.vs
 
