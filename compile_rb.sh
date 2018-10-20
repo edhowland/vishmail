5793d7f74b7edfd94a90c906896513215aa5e97e
Ed Howland <ed.howland@gmail.com>
2018-10-20 17:15:46 -0400

    corrected bug in compile_rb.sh where vishc needs -l before all but the final files. TODO: Must try and remove bload.vs

--- compile_rb.sh
+++ compile_rb.sh
@@ -2,6 +2,6 @@
 # compile_rb.sh vs files into .ibx.rb
 # Differs from h.sh in that removes *.rb files and adds startup.vs
 # Use run.sh to execute it
-vishc -R  -r ./util.rb -r ./msg.rb -r ./compose.rb -o ibx.rb fnlib.vs main.vs msg.vs inbox.vs insert.vs send.vs compose.vs  bload.vs startup.vs
+vishc -R  -r ./util.rb -r ./msg.rb -r ./compose.rb -o ibx.rb -l fnlib.vs -l main.vs -l msg.vs -l inbox.vs -l insert.vs -l send.vs -l compose.vs -l bload.vs startup.vs
 
 
